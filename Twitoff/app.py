d995a51e3f3492beba2952ac0b0c861a037d500c
ocalzada <ocalzada@users.noreply.github.com>
2019-10-15 19:38:01 -0600

    Add Twitoff app, models, templates and twitter files

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -1,18 +1,29 @@
 """Main application for twitoff"""
 
 #imports
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 #create app factory
 def create_app():
     """creates and configures an instance of a flask app"""
     app = Flask(__name__)
     
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_DATABASE_URI"] = config("DATABASE_URL")
+    app.config["ENV"] = config("ENV") # should change this later to production
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     DB.init_app(app)
 
     @app.route("/")
     def root():
-        return "Welcome to our app"
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    app.route("/reset")
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template("base.html", title="DB Reset", users=[])
     return app
