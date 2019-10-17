843fe25859d0a3f85edaa761e28a51a06d690b48
ocalzada <ocalzada@users.noreply.github.com>
2019-10-17 11:23:18 -0600

    Add Procfile

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -4,6 +4,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user
 
 #create app factory
@@ -22,7 +23,7 @@ def create_app():
         users = User.query.all()
         return render_template("base.html", title="Home", users=users)
 
-    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
         name = name or request.values['user_name']
@@ -35,6 +36,19 @@ def create_app():
             message = "Error addding {}: {}".format(name, e)
             tweets=[]
         return render_template('user.html', title=name, tweets=tweets, message=message)
+    
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                              request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     app.route("/reset")
     def reset():
