408d23c77786132228df94764d3a3b8be4d80420
ocalzada <ocalzada@users.noreply.github.com>
2019-10-17 09:07:29 -0600

    Update app, base, user and twitter files. Add predict file.

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -4,6 +4,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 #create app factory
 def create_app():
@@ -20,6 +21,20 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template("base.html", title="Home", users=users)
+
+    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method =='POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error addding {}: {}".format(name, e)
+            tweets=[]
+        return render_template('user.html', title=name, tweets=tweets, message=message)
 
     app.route("/reset")
     def reset():
